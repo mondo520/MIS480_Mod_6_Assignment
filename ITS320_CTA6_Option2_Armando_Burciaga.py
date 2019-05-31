62d10938b6687321d6b4d8b0b944aba91a43d21b
mondo520 <51217609+mondo520@users.noreply.github.com>
2019-05-30 19:31:07 -0500

    Add files via upload

---
+++ ITS320_CTA6_Option2_Armando_Burciaga.py
@@ -0,0 +1,10 @@
+A = [1,2]
+B = [3,4]
+
+cart=[]
+for val1 in A: #loop for list A
+  for val2 in B: #loop for list B
+    cart.append((val1,val2)) #appending each tuple to list
+     
+print(cart) #display
+
